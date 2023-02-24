8eb29aeb23f4b85050d4bd3b3537564495b64c13
ksinghj <kartarsjabanda@gmail.com>
2023-02-24 17:34:48 +0100

    first commit

---
+++ src/lib/NameInput.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte'
+
+  const dispatch = createEventDispatcher()
+
+  export let value = ''
+
+  const handleChange = e =>
+    dispatch('valueChange', {
+      value: e.target.value
+    })
+</script>
+
+<input type="text" {value} on:input={handleChange} />
